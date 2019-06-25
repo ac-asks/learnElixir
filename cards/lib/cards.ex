b76f8a8f864463554808b7225e08530c3d318d92
April <aprilchi4@gmail.com>
2019-06-26 01:25:39 +1000

    added save method using builtin method

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,3 +1,4 @@
+# index notation to get something out of an array will NOT WORK
 defmodule Cards do
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
@@ -24,6 +25,19 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size) # this returns a tuple with {[deck:[], hand:[]}
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck) # invoke erlang code here - converts it into something that can be written into file system
+    File.write(filename, binary)
+  end
+
+
+ # pattern matching is Elixir's replacement for variable assignment
+ # assignment is the simplest form of pattern matching in Elixir i.e. deck = Cards.create_deck
+
+# pattern matching:
+#  e.g. {hand, rest_of_dec} = Cards.deal(deck, 5) WORKS
+#  because the left and right hand side both have the same data structure and have equal number of elements in the tuple
 
 
 end
